3640b11b5945d2e2f73ff1360ba57eea7136ec4b
Mikhail Zabolotko <zabolotko@gmail.com>
2022-01-03 22:51:47 +0300

    Do few step

--- config.el
+++ config.el
@@ -34,99 +34,6 @@
 
 ;; (require 'config-elpa)
 (require 'config-use-package)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; Enable richer annotations using the Marginalia package
-(use-package marginalia
-  ;; Either bind `marginalia-cycle` globally or only in the minibuffer
-  :bind (;; ("M-A" . marginalia-cycle)
-         :map minibuffer-local-map
-         ("M-A" . marginalia-cycle))
-  ;; The :init configuration is always executed (Not lazy!)
-  :init
-  ;; Must be in the :init section of use-package such that the mode gets
-  ;; enabled right away. Note that this forces loading the package.
-  (marginalia-mode))
-
-(use-package embark
-  :bind
-  (("C-." . embark-act)))
-
-;; Enable vertico
-(use-package vertico
-  :init
-  (vertico-mode)
-  ;; Grow and shrink the Vertico minibuffer
-  ;; (setq vertico-resize t)
-
-  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
-  (setq vertico-cycle t))
-
-;; Optionally use the `orderless' completion style. See
-;; `+orderless-dispatch' in the Consult wiki for an advanced Orderless style
-;; dispatcher. Additionally enable `partial-completion' for file path
-;; expansion. `partial-completion' is important for wildcard support.
-;; Multiple files can be opened at once with `find-file' if you enter a
-;; wildcard. You may also give the `initials' completion style a try.
-(use-package orderless
-  :init
-  ;; Configure a custom style dispatcher (see the Consult wiki)
-  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
-  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
-  (setq completion-styles '(orderless)
-        completion-category-defaults nil
-        completion-category-overrides '((file (styles partial-completion)))))
-
-(use-package ripgrep)
-
-(use-package dash)
-
-(use-package f)
-
-(use-package org)
-
-(use-package emacsql)
-
-(use-package emacsql-sqlite)
-
-(use-package magit-section)
-
-(use-package org-roam
-  :after (dash f org emacsql emacsql-sqlite magit-section)
-  :init
-  (setq org-roam-v2-ack t)
-  :custom
-  (org-roam-directory "~/Documents/org/Documents/Roam")
-  (org-roam-node-display-template "${title:80} ${tags:60}")
-  (org-roam-capture-templates
-    '(("d" "default" plain "%?" :if-new
-       (file+head "%<%Y%m%d%H%M%S>.org" "#+TITLE: ${title}\n#+CREATED: %U\n#+LAST_MODIFIED: %U\n#+ROAM_ALIASES: \n#+FILETAGS: ")
-       :unnarrowed t)))
-
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
-         ("C-c n i" . org-roam-node-insert)
-         :map org-mode-map
-         ("C-M-i"   . completion-at-point))
-  :config
-  (org-roam-db-autosync-enable))
-
-(use-package org-roam-ui
-  :after org-roam
-  :straight
-    (:host github :repo "org-roam/org-roam-ui" :branch "main" :files ("*.el" "out"))
-  :config
-  (setq org-roam-ui-sync-theme t
-        org-roam-ui-follow t
-        org-roam-ui-update-on-save t
-        org-roam-ui-open-on-start t))
-
-(use-package magit
-  :after (dash git-commit transient with-editor)
-  :commands magit)
-
-
 ;; (require 'config-view)
 ;; (require 'config-xml)
 ;; (require 'config-calendar)
@@ -135,7 +42,7 @@
 ;; (require 'config-buffer)
 ;; ;; (require 'config-fsharp)
 ;; ;; (require 'config-csharp)
-;; (require 'config-packages)
+(require 'config-packages)
 
-;; (if (file-exists-p custom-file)
-;;   (load custom-file))
+(if (file-exists-p custom-file)
+  (load custom-file))
